4afcec82670dac4da1b1c984c38ab42861cd3b14
oreotamish <sinhatamish@gmail.com>
2023-10-16 18:55:44 +0530

    fixed return err rust

--- projects/rust-ms/src/main.rs
+++ projects/rust-ms/src/main.rs
@@ -4,7 +4,8 @@ use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 fn write_green() -> io::Result<()> {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
-    writeln!(&mut stdout, "Hello Earthly!")
+    writeln!(&mut stdout, "Hello Earthly!")?;
+    Ok(())
 }
 
 fn main() {

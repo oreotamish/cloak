e5d3fd3eab40e3466e966a41ffdb2a48fc3d5914
oreotamish <sinhatamish@gmail.com>
2023-10-17 10:16:55 +0530

    Feat: fixed earthfiles

--- Earthfile
+++ Earthfile
@@ -3,3 +3,4 @@ VERSION 0.7
 all:
     BUILD ./projects/go-ms+all
     BUILD ./projects/rust-ms+all
+    BUILD ./projects/ts-ms+all
--- projects/rust-ms/src/main.rs
+++ projects/rust-ms/src/main.rs
@@ -4,7 +4,7 @@ use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 fn write_green() -> io::Result<()> {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
-    writeln!(&mut stdout, "Hello Earthly!")?;
+    writeln!(&mut stdout, "Hello World!")?;
     Ok(())
 }
 
